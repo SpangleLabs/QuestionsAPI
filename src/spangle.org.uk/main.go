fc2ec00efe81736dc09302c0c8189fd0227a2d55
Joshua Coales <joshua@coales.co.uk>
2016-09-06 16:07:55 +0100

    Splitting the router out from main.go and improving it

--- src/spangle.org.uk/main.go
+++ src/spangle.org.uk/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"spangle.org.uk/questions"
@@ -9,10 +8,7 @@ import (
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", questions.Index)
-	router.HandleFunc("/questions", questions.QuestionIndex)
-	router.HandleFunc("/questions/{questId}", questions.QuestionShow)
+	router := questions.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
