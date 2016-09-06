0c7c2b9ed1f3258fa1108c6810fa73aeeb1fb8bc
Joshua Coales <joshua@coales.co.uk>
2016-09-06 13:58:21 +0100

    Adding json ID tags to the Question struct

--- src/spangle.org.uk/question.go
+++ src/spangle.org.uk/question.go
@@ -3,11 +3,11 @@ package spangle_org_uk
 import "time"
 
 type Question struct {
-	Id int
-	Text string
-	Period time.Duration
-	Start time.Time
-	ShowLast bool
+	Id		int		`json:"id"`
+	Text		string		`json:"text"`
+	Period		time.Duration	`json:"period"`
+	Start		time.Time	`json:"start"`
+	ShowLast	bool		`json:"show_last"`
 }
 
 type Questions []Question
