5b4b2c8d73268b3a192b66ba9f4d43eb583af7f6
Joshua Coales <joshua@coales.co.uk>
2016-09-06 15:16:36 +0100

    Splitting questionsAPI file between main.go and handlers.go

--- src/spangle.org.uk/main.go
+++ src/spangle.org.uk/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"spangle.org.uk/questions"
+)
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", questions.Index)
+	router.HandleFunc("/questions", questions.QuestionIndex)
+	router.HandleFunc("/questions/{questId}", questions.QuestionShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
--- src/spangle.org.uk/questions/handlers.go
+++ src/spangle.org.uk/questions/handlers.go
@@ -1,1 +1,28 @@
 package questions
+
+import (
+	"net/http"
+	"spangle.org.uk/questions/model"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func QuestionIndex(w http.ResponseWriter, r *http.Request) {
+	questionList := questions.Questions{
+		questions.Question{Id: 1, Text: "Test question?"},
+		questions.Question{Id: 2, Text: "Testing again?"},
+	}
+
+	json.NewEncoder(w).Encode(questionList)
+}
+
+func QuestionShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	questId := vars["questId"]
+	fmt.Fprintln(w, "Question show:", questId)
+}
