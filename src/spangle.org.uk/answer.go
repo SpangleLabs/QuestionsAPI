f4e764f27ff5452a2f300adbbb5aa76a694dc922
Joshua Coales <joshua@coales.co.uk>
2016-09-06 14:35:29 +0100

    Adding json tags to Answer struct

--- src/spangle.org.uk/answer.go
+++ src/spangle.org.uk/answer.go
@@ -3,10 +3,10 @@ package spangle_org_uk
 import "time"
 
 type Answer struct {
-	Id int
-	Date time.Time
-	Question Question
-	Text string
+	Id		int		`json:"id"`
+	Date		time.Time	`json:"date_time"`
+	Question	Question	`json:"question"`
+	Text		string		`json:"text"`
 }
 
 type Answers []Answer
